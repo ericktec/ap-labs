dfda5c87a4dd59bd065c023d5dc35924ea52c117
ericktec <a01631093@itesm.mx>
2020-09-02 15:08:30 -0500

    strlib

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+int stringComparison(char *first, char *second);
+
+int main(int argc, char **argv) {
+    
+    if(argc < 2)
+    {
+        printf("There is not operation defined \n");
+        return 0;
+    }
+    else if(stringComparison(argv[1], "-add") && argc < 4)
+    {
+        printf("Failed addition \n");
+        return 0;
+    }
+    else if (stringComparison(argv[1], "-find") && argc < 4)
+    {
+        printf("Failed find \n");
+        return 0;
+    }
+
+    if(stringComparison(argv[1], "-add")){
+        printf("Initial Length  : %d \n", mystrlen(argv[2]));
+        char * newString = mystradd(argv[2], argv[3]);
+        printf("New String  : %s \n",newString);
+        printf("New Length  : %d \n", mystrlen(newString));
+    }else if(stringComparison(argv[1], "-find"))
+    {
+        printf("[%s] string was found at [%d] position \n", argv[3], mystrfind(argv[2],argv[3]));
+    }
+    
     return 0;
 }
+
+
+
+
+
+
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str)
+{
+    int tam = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        tam++;
+    }
+    return tam;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition)
+{
+    int i = 0;
+    int j = 0;
+    char * temp =(char *)malloc(mystrlen(origin)+mystrlen(addition));
+    while (origin[i] != '\0')
+    {
+        temp[i] = origin[i];
+        i++;
+    }
+    while (addition[j] != '\0')
+    {
+        temp[i] = addition[j];
+        i++;
+        j++;
+    }
+    temp[i] = '\0';
+    return temp;
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int mystrfind(char *origin, char *substr)
+{
+    int subCounter = 0;
+    int length = mystrlen(substr);
+    int answer = -1;
+    for(int i = 0; origin[i]!='\0'; i++)
+    {
+        if(origin[i] == substr[subCounter])
+        {
+            if(subCounter == 0){
+                answer = i;
+            }
+            subCounter++;
+        }else
+        {
+            subCounter = 0;
+            answer = -1;
+        }
+
+        if(subCounter == length){
+            return answer;
+        }
+    }
+    return answer;
 }
+
+
+int stringComparison(char *first, char *second)
+{
+    if(mystrlen(first) == mystrlen(second)){
+        for (int i = 0; first[i] != '\0'; i++)
+        {
+            if(first[i] != second[i]){
+                return 0;
+            }
+        }
+        
+    }else
+    {
+        return 0;
+    }
+    return 1;
+    
+}
+
+
