1e8175c21ec1560602bec46370f4316d9135c1d5
ericktec <a01631093@itesm.mx>
2020-09-17 18:16:59 -0500

    go-slices-maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Split(s, " ")
+	lenOfWords := make(map[string]int)
+
+	for i:=0; i<len(words);i++{
+		lenOfWords[words[i]] += 1 
+	}
+	return lenOfWords
+}
+
 func main() {
+	wc.Test(WordCount)
 }
