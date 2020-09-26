70fa512a26cfd2cf50f3d6b7116f9a141084d107
ericktec <a01631093@itesm.mx>
2020-09-26 13:58:12 -0500

    advance-logger

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -2,34 +2,44 @@
 #include <stdlib.h>
 #include "logger.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
+	int option;
+	if(argc==1)
+	{
+		option = 0;
+	}else
+	{
+		option = atoi(argv[1]);
+	}
+	
+	printf("%d\n", option);
+	switch (option)
+	{
+	case 1:
+		// default logging
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+	case 2:
+		// stdout logging
+		initLogger("stdout");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+	case 3:
+		// syslog logging
+		initLogger("syslog");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+	default:
+		errorf("Invalid test case");
+		break;
+	}
 
-    printf("%d\n", atoi(argv[1]));
-    switch(atoi(argv[1])) {
-    case 1:
-	// default logging
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    default:
-	errorf("Invalid test case");
-	break;
-    }
-
-    return 0;
+	return 0;
 }
