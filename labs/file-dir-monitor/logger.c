6d3ba94c2518464def4cf170e4fecd9e5a95fa79
ericktec <a01631093@itesm.mx>
2020-10-04 14:16:30 -0500

    file-dir-monitor

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,131 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 #include "logger.h"
+#include <syslog.h>
+#include <stdarg.h>
 
-int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+
+#define RESET 0
+#define BRIGHT 1
+#define DIM 2
+#define UNDERLINE 3
+#define BLINK 4
+#define REVERSE 7
+#define HIDDEN 8
+
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+
+int sysLogMode = 0;
+
+void textcolor(int attr, int fg, int bg)
+{
+    char command[13];
+    sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+    printf("%s", command);
+}
+
+int initLogger(char *logType)
+{
+    if(strcmp(logType, "syslog") == 0)
+    {
+        printf("Initializing Logger on: %s\n", logType);
+        sysLogMode = 1;
+    }
+    else if( (strcmp(logType,"stdout")==0) || (strcmp(logType, "")==0) ){
+        sysLogMode = 0;
+    }
+    else
+    {
+        printf("Invalid option, using default stdout mode\n");
+        sysLogMode = 0;
+    }
+    
     return 0;
 }
 
-int infof(const char *format, ...) {
+int infof(const char *format, ...)
+{
+    va_list arguments;
+    va_start(arguments, format);
+    if(sysLogMode == 1)
+    {
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_CONS);
+        vsyslog(LOG_INFO, format, arguments);
+        closelog();
+    }
+    else
+    {
+        vprintf(format, arguments);
+    }
+    va_end(arguments);
     return 0;
+}
+int warnf(const char *format, ...)
+{
+    va_list arguments;
+    va_start(arguments, format);
+    if(sysLogMode == 1)
+    {
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_CONS);
+        vsyslog(LOG_WARNING, format, arguments);
+        closelog();
+    }
+    else
+    {
+        textcolor(BRIGHT, YELLOW, BLACK);
+        vprintf(format, arguments);
+        textcolor(RESET, WHITE, BLACK);
+    }
+    va_end(arguments);
+    
+    return 0;
+}
+int errorf(const char *format, ...)
+{
+    va_list arguments;
+    va_start(arguments, format);
+    if(sysLogMode == 1)
+    {
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_CONS);
+        vsyslog(LOG_ERR, format, arguments);
+        closelog();
+    }
+    else
+    {
+        textcolor(RESET, RED, BLACK);
+        vprintf(format, arguments);
+        textcolor(RESET, WHITE, BLACK);
+    }
+    va_end(arguments);
+    
+    return -1;
+}
+int panicf(const char *format, ...)
+{
+    va_list arguments;
+    va_start(arguments, format);
+    if(sysLogMode == 1)
+    {
+        openlog("Logger", LOG_PID | LOG_CONS, LOG_CONS);
+        vsyslog(LOG_EMERG, format, arguments);
+        closelog();
+    }
+    else
+    {
+        textcolor(BRIGHT, MAGENTA, BLACK);
+        vprintf(format, arguments);
+        textcolor(RESET, WHITE, BLACK);
+    }
+    va_end(arguments);
+    return -1;
 }
 
-int warnf(const char *format, ...) {
-    return 0;
-}
-
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
-}
 
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,234 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "logger.h"
+#include <string.h>
+#include <limits.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <ftw.h>
+#include "uthash.h"
 
-int main(){
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+
+int inotifyFd;
+char *rootPath;
+char *oldPath;
+int rootId;
+
+
+void handler(struct inotify_event *event);
+int createWatcher(char *path);
+int addFolder(const char *fpath, const struct stat *sb, int typeflag);
+void updateFolder(char *oldPath, char* newPath);
+
+
+struct Watchers
+{
+    int id;
+    char path[NAME_MAX + 1];
+    UT_hash_handle hh;
+};
+
+struct FTW {
+    int base;
+    int level;
+};
+
+struct Watchers *watchers = NULL;
+
+int main(int argc, char *argv[])
+{
     // Place your magic here
+    if (argc != 2)
+    {
+        errorf("Wrong number of parameters\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int wd;
+    inotifyFd = inotify_init();
+    char buf[BUF_LEN] __attribute__((aligned(8)));
+    ssize_t numRead;
+    char *p;
+    struct inotify_event *event;
+
+    if (inotifyFd < 0)
+    {
+        errorf("Something went wrong initializing notify\n");
+        exit(EXIT_FAILURE);
+    }
+
+    infof("Starting File/Directory Monitor on %s \n -----------------------------------------------------\n", argv[1]);
+    rootPath = argv[1];
+    oldPath = rootPath;
+    wd = inotify_add_watch(inotifyFd, argv[1], IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
+    if (wd == -1)
+    {
+        errorf("Error in the path given \n");
+        exit(EXIT_FAILURE);
+    }
+    struct Watchers *s;
+
+    s = malloc(sizeof(struct Watchers));
+    s->id = wd;
+    rootId = wd;
+    strcpy(s->path, rootPath);
+    HASH_ADD_INT(watchers, id, s);
+
+    struct FTW *flag = malloc(sizeof(struct FTW));
+    flag->base = 0;
+    flag->level = 1;
+
+    if (nftw(argv[1], addFolder, 20, NULL,  flag) == -1)
+    {
+        errorf("Something went wrong while creating the watcher.\n");
+    }
+
+    
+    for (;;)
+    { /* Read events forever */
+        numRead = read(inotifyFd, buf, BUF_LEN);
+        if (numRead == 0)
+            return (panicf("Error getting information from the buffer \n"));
+
+        if (numRead == -1)
+        {
+            errorf("Something went wrong while reading the events\n");
+        }
+
+        for (p = buf; p < buf + numRead;)
+        {
+            event = (struct inotify_event *)p;
+            handler(event);
+
+            p += sizeof(struct inotify_event) + event->len;
+        }
+    }
+
+    inotify_rm_watch(inotifyFd, wd);
+    close(inotifyFd);
+
+    exit(EXIT_SUCCESS);
+}
+
+void handler(struct inotify_event *event)
+{
+    
+    if (event->mask & IN_CREATE)
+    {
+        struct Watchers *s;
+        HASH_FIND_INT(watchers, &event->wd, s);
+        if (event->mask & IN_ISDIR)
+        {
+            char *path = s->path;
+            strcat(path, "/");
+            strcat(path, event->name);
+            int wd = createWatcher(path);
+            struct Watchers *n;
+
+            n = malloc(sizeof(struct Watchers));
+            n->id = wd;
+            strcpy(n->path, path);
+            HASH_ADD_INT(watchers, id, n);
+            infof("- [Directory - Create] - %s\n", path);
+        }
+        else
+        {
+            
+            if (rootId == s->id)
+            {
+                infof("- [File - Create] - %s\n", event->name);
+            }
+            else
+            {
+                infof("- [File - Create] - %s/ %s\n", s->path, event->name);
+            }
+        }
+    }
+    if (event->mask & IN_DELETE)
+    {
+        if (event->mask & IN_ISDIR)
+        {
+            struct Watchers *s;
+            HASH_FIND_INT(watchers, &event->wd, s);
+            infof("- [Directory - Removal] - %s\n", event->name);
+            if (rootId != s->id)
+            {
+                inotify_rm_watch(inotifyFd, event->wd);
+            }
+        }
+        else
+        {
+            infof("- [File - Removal] - %s\n", event->name);
+        }
+    }
+    if (event->mask & IN_MOVED_FROM)
+    {
+        struct Watchers *s;
+        HASH_FIND_INT(watchers, &event->wd, s);
+        
+        if (event->mask & IN_ISDIR)
+        {
+            infof("- [Directory Rename] %s/ %s", s->path,event->name);
+            strcpy(oldPath, s->path);
+            strcat(oldPath, "/");
+            strcat(oldPath, event->name);
+        }
+        else
+        {
+            infof("- [File Rename] %s/ %s", s->path,event->name);
+        }
+    }
+    if (event->mask & IN_MOVED_TO)
+    {
+        struct Watchers *s;
+        HASH_FIND_INT(watchers, &event->wd, s);
+        if (event->mask & IN_ISDIR)
+        {
+            char *newPath = s->path;
+            strcat(newPath, "/");
+            strcat(newPath, event->name);
+            warnf("Old path %s, newPath %s current id %d\n", oldPath, newPath, event->wd);
+            updateFolder(oldPath, newPath);
+            strcpy(oldPath, "");
+        }
+
+        infof("-> %s/ %s\n", s->path, event->name);
+    }
+}
+
+int createWatcher(char *path)
+{
+    int wd = inotify_add_watch(inotifyFd, path, IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
+    return wd;
+}
+
+int addFolder(const char *fpath, const struct stat *sb, int typeflag)
+{
+    if (typeflag == FTW_D)
+    {
+        int wd = createWatcher(fpath);
+        if(wd == rootId){
+            return 0;
+        }
+        struct Watchers *s;
+        s = malloc(sizeof(struct Watchers));
+        s->id = wd;
+        strcpy(s->path, fpath);
+        HASH_ADD_INT(watchers, id, s);
+    }
     return 0;
+}
+
+
+void updateFolder(char *oldPath, char* newPath) {
+    struct Watchers *s;
+
+    for(s=watchers; s != NULL; s = s->hh.next) {
+        if(strcmp(oldPath, s->path) == 0)
+        {
+            strcpy(s->path, newPath);
+            warnf("sucess");
+        }
+    }
 }
