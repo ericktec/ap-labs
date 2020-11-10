6705d0f53c1ecf7af1636a3743123d95e5e563fc
ericktec <a01631093@itesm.mx>
2020-11-09 20:11:34 -0600

    goroutines-vs-os-threads finish

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var commsPerSecond int
+	waitingChannel := make(chan struct{})
+	q := make(chan int)
+	var i int64
+	start := time.Now()
+	go func() {
+		q <- 1
+		for {
+			i++
+			q <- <-q
+		}
+		waitingChannel <- struct{}{}
+	}()
+	go func() {
+		for {
+			i++
+			q <- <-q
+		}
+		waitingChannel <- struct{}{}
+	}()
+	for {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Println("Communications Per Second : ", float64(i)/time.Since(start).Seconds())
+		}
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+	}
+
+	<-waitingChannel
+
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var waitingChannel = make(chan struct{})
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+var startingTime = time.Now()
+
+func pipeline(in chan struct{}, out chan struct{}, current int, limit int) {
+	if current <= limit {
+		fmt.Println("Maximum number of pipeline stages   : ", current)
+		fmt.Println("Time to transit trough the pipeline : ", time.Since(startingTime))
+		go pipeline(out, in, current+1, limit)
+		out <- <-in
+	} else {
+		waitingChannel <- struct{}{}
+	}
+}
+
+func main() {
+	var in chan (struct{})
+	var out chan (struct{})
+	go pipeline(in, out, 0, 10000000)
+	<-waitingChannel
+
 }
